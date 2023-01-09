4f7ea43600f86dd2ba866cc021a4dd4df01103ff
94__m_u_k <53416900+kong759@users.noreply.github.com>
2023-01-09 10:41:17 +0900

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -3,8 +3,8 @@
 #테라폼은 멀티 플랫폼 서비스를 지원하므로 프로바이더를 선택하여 인프라를 구축 할 수 있으며 현 프로젝트는 AWS로 진행.
 provider "aws" {
   region = "us-east-2"
-  access_key = "AKIAQIRZSFDXULE7JU5H"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
 
 provider "http" {}
